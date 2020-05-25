859a3fe3aea0cec8546c8e785a3336ce4187c7cd
Felix-QC <you@example.com>
2020-05-25 09:31:55 -0400

    Update fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+--[[
+█▀▄▀█ █▀▀█ █▀▀▄ █▀▀   █▀▀▄ █░░█   █▀▀ █▀▀ █░░ ░▀░ █░█
+█░▀░█ █▄▄█ █░░█ █▀▀   █▀▀▄ █▄▄█   █▀▀ █▀▀ █░░ ▀█▀ ▄▀▄
+▀░░░▀ ▀░░▀ ▀▀▀░ ▀▀▀   ▀▀▀░ ▄▄▄█   ▀░░ ▀▀▀ ▀▀▀ ▀▀▀ ▀░▀
+--]]
+
+fx_version 'bodacious'
+games 'gta5'
+client_script 'client.lua'
+author 'Felix-QC'
+version "2.1.0"
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/app.js',
+	'html/style.css',
+	'img/mic.png'
+}
+
